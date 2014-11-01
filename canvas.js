c088ed419c634d24e41a0b03319eb33e957c39cd
Chase Blokker <chaseblokker@yahoo.com>
2014-11-01 16:11:17 -0400

    staging for merge - again

--- canvas.js
+++ canvas.js
@@ -69,7 +69,7 @@ controller.on('frame', function(frame) {
     for (var i = 0; i < frame.hands.length; i++) {
         // For each hand we define it
         var hand = frame.hands[i],
-            handPos = leapToScene(frame, hand.palmPosition),
+            handPos = hand.palmPosition,
             handPitch = hand.pitch(),
             handRoll = hand.roll(),
             handYaw = hand.yaw(),
