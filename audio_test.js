f084ae1ddd9d4dc7db1f4b02e9d4817225d33ba9
Chase Blokker <chaseblokker@yahoo.com>
2014-11-01 14:17:36 -0400

    refactor

--- audio_test.js
+++ audio_test.js
@@ -1,7 +1,6 @@
 // Creates our Leap Controller
 var controller = new Leap.Controller();
 
-
 var note = new synthNote(200);
 note.makeMeAnOsc();
 var synth = new analyzeDest([note]);
@@ -9,14 +8,8 @@ synth.play(0);
 
 
 // Tells the controller what to do every time it sees a frame
-controller.on('frame', function(frame) {
-    //Clears the canvas so we are not drawing multiple frames   
-    // c.clearRect(0, 0, width, height);
-
-    
-    // First we loop through all of the hands that the frame sees
+controller.on('frame', function(frame) {    
     for (var i = 0; i < frame.hands.length; i++) {
-        // For each hand we define it
         var hand = frame.hands[i],
             handPos = hand.palmPosition,
             handPitch = hand.pitch(),
@@ -31,28 +24,8 @@ controller.on('frame', function(frame) {
 
         synth.updateFreq(freq, 0);
         
-        // console.log(handPos);
-        // console.log(handPitch);
-        // console.log(handRoll);
-        // console.log(handYaw);
-
-        handColor = Math.floor(Math.abs(handPitch * 200));
-        // c.fillStyle = 'rgb(' + handColor + ', 100, 0)';
-        
-        // // Creating the path for the hand circle
-        // c.beginPath();
-        
-        // Draw a circle at the hand position
-        // c.arc(
-        //     handPos[0], handPos[1], handPos[2],
-        //     Math.PI - Math.PI * handRoll / 2,
-        //     -(Math.PI * hand.roll() / 2)
-        // );
-        
-        // c.closePath();
-        // c.fill();
     }
 });
 
-// Get frames rolling by connecting the controller
+
 controller.connect();
