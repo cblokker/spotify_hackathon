d5c35276d32b7069ad903c74753f4de924f53c7c
Chase Blokker <chaseblokker@yahoo.com>
2014-11-01 15:42:45 -0400

    playing with synth models

--- audio_test.js
+++ audio_test.js
@@ -1,4 +1,7 @@
-// Creates our Leap Controller
+var controller = new Leap.Controller();
+var note = new synthNote(200);
+note.makeMeAnOsc();
+var synth = new analyzeDest([note]);
 
 
 function convertToRange(val, in_range, out_range) {
@@ -7,16 +10,6 @@ function convertToRange(val, in_range, out_range) {
 	var out_min = out_range[0], out_max = out_range[1];
 	return (val - in_min) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
 }
-
-
-
-var controller = new Leap.Controller();
-
-var note = new synthNote(200);
-note.makeMeAnOsc();
-var synth = new analyzeDest([note]);
-
-
 
 
 // Tells the controller what to do every time it sees a frame
@@ -34,17 +27,42 @@ controller.on('frame', function(frame) {
             handRoll = hand.roll(),
             handYaw = hand.yaw();
 
-        var freq = convertToRange(handPos[1], [0, 400], [200, 3000])
-        var gain = convertToRange(handPos[2], [-200, 300], [1, 0])
+        var freq = convertToRange(handPos[1], [0, 400], [200, 3000]);
+        var gain = convertToRange(handPos[2], [-200, 300], [1, 0]);
 
-        console.log(handPos[2]);
+        console.log(handPos[0]);
 
         synth.updateGain(gain, 0);
         synth.updateFreq(freq, 0);
+
+        if (handPos[0] < 0) {
+            synth.squareWave(0);
+        } else {
+            synth.sinWave(0);
+        }
     }
 });
 
-
+// var controller = Leap.loop({enableGestures: true}, function(frame){
+//   if(frame.valid && frame.gestures.length > 0){
+//     frame.gestures.forEach(function(gesture){
+//         switch (gesture.type){
+//           case "circle":
+//               console.log("Circle Gesture");
+//               break;
+//           case "keyTap":
+//               console.log("Key Tap Gesture");
+//               break;
+//           case "screenTap":
+//               console.log("Screen Tap Gesture");
+//               break;
+//           case "swipe":
+//               console.log("Swipe Gesture");
+//               break;
+//         }
+//     });
+//   }
+// });
 
 
 
