6178281ce1cb7a2684603585869df789306da3ba
Chase Blokker <chaseblokker@yahoo.com>
2014-11-01 17:59:06 -0400

    two hands can make notes

--- audio_test.js
+++ audio_test.js
@@ -1,70 +1,91 @@
 var controller = new Leap.Controller();
-var note = new synthNote(200);
-note.makeMeAnOsc();
-var synth = new playback([note]);
+
+var note1 = new synthNote(200);
+var note2 = new synthNote(200);
+var note3 = new synthNote(200);
+
+note1.makeMeAnOsc();
+note2.makeMeAnOsc();
+note3.makeMeAnOsc();
+
+var synthArray = [
+  new playback([note1]),
+  new playback([note2]),
+  new playback([note3])
+]
 
 var sample = new Sample('audios/Laugh-Evil1.mp3');
-sample.loadBuffer();
+    sample.loadBuffer();
+
+
+var controller = Leap.loop({enableGestures: true}, function(frame) {
+    handGestureNoises(frame);
+    synthHands(frame);
+});
 
 
 function convertToRange(val, in_range, out_range) {
-	// ranges are 2 value arrays, in min + max, out min + max
-	var in_min = in_range[0], in_max = in_range[1]
-	var out_min = out_range[0], out_max = out_range[1];
-	return (val - in_min) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
+    // ranges are 2 value arrays, in min + max, out min + max
+    var in_min = in_range[0], in_max = in_range[1]
+    var out_min = out_range[0], out_max = out_range[1];
+    return (val - in_min) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
 }
 
 
-// Tells the controller what to do every time it sees a frame
-controller.on('frame', function(frame) {
+function synthHands(frame) {
     if (frame.hands.length == 0) {
-        synth.stop(0);
+        synthArray[0].stop(0);
+        synthArray[1].stop(0);
     } else {
-        synth.play(0);
+        synthArray[0].play(0);
+        synthArray[1].play(0);
     }
 
-    if(frame.valid && frame.gestures.length > 0){
-        frame.gestures.forEach(function(gesture){
-            switch (gesture.type) {
-                case "circle":
-                    console.log("Circle Gesture");
-                    break;
-                case "keyTap":
-                    console.log("Key Tap Gesture");
-                    break;
-                case "screenTap":
-                    console.log("Screen Tap Gesture");
-                    break;
-                case "swipe":
-                    console.log('swipe');
-                    sample.play(0);
-                    break;
-            }
-        });
-    }
 
     for (var i = 0; i < frame.hands.length; i++) {
         var hand = frame.hands[i],
             handPos = hand.palmPosition,
             handPitch = hand.pitch(),
             handRoll = hand.roll(),
-            handYaw = hand.yaw();
+            handYaw = hand.yaw(),
 
-        var freq = convertToRange(handPos[1], [0, 400], [200, 3000]);
-        var gain = convertToRange(handPos[2], [-200, 300], [1, 0]);
+            freq = convertToRange(handPos[1], [0, 400], [200, 3000]),
+            gain = convertToRange(handPos[2], [-200, 300], [1, 0]);
 
-        console.log(handPos[0]);
-
-        synth.updateGain(gain, 0);
-        synth.updateFreq(freq, 0);
+        synthArray[i].updateGain(gain, 0);
+        synthArray[i].updateFreq(freq, 0);
 
         if (handPos[0] < 0) {
-            synth.squareWave(0);
+            synthArray[i].squareWave(0);
         } else {
-            synth.sinWave(0);
+            synthArray[i].sinWave(0);
         }
     }
-});
+}
+
+
+function handGestureNoises(frame) {
+    if (frame.valid && frame.gestures.length > 0){
+        frame.gestures.forEach(function(gesture){
+            switch (gesture.type){
+              case "circle":
+                  console.log("Circle Gesture");
+                  break;
+              case "keyTap":
+                  console.log("Key Tap Gesture");
+                  break;
+              case "screenTap":
+                  console.log("Screen Tap Gesture");
+                  break;
+              case "swipe":
+                  console.log("Swipe Gesture");
+                  sample.play(0);
+                  break;
+            }
+        });
+    }
+}
+
 
 
 controller.connect();
